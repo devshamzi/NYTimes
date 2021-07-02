71cf4ab6407c098c3924f02d51eb45984e9d3d2f
Shamseer Ali <shamzi.erz@hotmail.com>
2021-07-02 21:25:07 +0400

    Add : Unit Test for API response

--- NYTimes/NYTimesTests/NYTimesTests.swift
+++ NYTimes/NYTimesTests/NYTimesTests.swift
@@ -6,12 +6,15 @@
 //
 
 import XCTest
+import Alamofire
+
 @testable import NYTimes
 
 class NYTimesTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDownWithError() throws {
@@ -21,6 +24,7 @@ class NYTimesTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
     }
 
     func testPerformanceExample() throws {
@@ -28,6 +32,34 @@ class NYTimesTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testApiCallCompletes() throws {
+        // given
+        let urlString = BaseURL.ApiBaseUrl.url
+        let url = URL(string: urlString)!
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+        
+        // when
+        var headers: HTTPHeaders?
+        
+        AF.request(url, method: .get, parameters:  nil , encoding: JSONEncoding.default, headers: headers).responseData{  response in
+            responseError =  response.error
+            
+            statusCode = response.response?.statusCode ?? 0
+            if statusCode == 200{
+                promise.fulfill()
+            }else{
+                XCTFail("Status code: \(statusCode ?? 0)")
+            }
+        }
+        wait(for: [promise], timeout: 5)
+        
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
     }
 
 }
